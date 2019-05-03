5727023195cb8c344e185c37830fc1f9c253eb95
rosalesdc <rosales9146@gmail.com>
2019-05-03 10:31:13 -0500

    [REF] modificaciones

--- vistas_contactos/models/contactos_clientes.py
+++ vistas_contactos/models/contactos_clientes.py
@@ -3,23 +3,29 @@
 from odoo import fields
 from odoo import models
 
-class LineasCuentaAnalitica(models.Model):
+class ContactosClientes(models.Model):
     _inherit = 'res.partner'
+    # def _get_partner_domain(self):
+    #     res = {}
+    #     contactos=self.env['res.partner'].search([('customer', '=', 'True')])
+    #     for contacto in contactos:
+    #
+    #
+    #     for orden in self.browse():
+    #         partners = []
+    #         string = ''
+    #         for i in partners:
+    #             string = string + str(i) + ','
+    #         res[orden.id] = '[' + string + ']'
+    #     return res
+
+    name=fields.Char(groups='ihm_ocultar_validar.group_no_editarcrear')
+    partner_domain = fields.Char(string="dominio", default="[557,1413]")
     
-    def _get_partner_domain(self):
-        #esta funcion devolvera una cadena de tecto de tipo “[1,34,23,45]” con los ids dela tabla res.partner que queremos mostrar.
-        res = {}
-        for orden in self.browse():
-            partners = []
-            #Realizamos las consultas necesarias para obtener los Ids que buscamos y los pasamos a la lista “partners”
-            #depues debemos pasar la lista a una cadena de texto, existen varias formas.. esta es una de ellas:
-            string = ''
-            for i in partners:
-                string = string + str(i) + ','
-            res[orden.id] = '[' + string + ']'
-        return res
-
-
+    partner_id = fields.Many2one(
+                                 'res.partner',
+                                 domain="[('id','=',8 )]")
+    
 #    _columns = {
 #        'partner_domain': fields.function(_get_partner_domain, method=True, type='char', string='Domain'),
 #        'partner_id': fields.many2one('res.partner', 'Partner', required=False, domain="[('id','in',eval(partner_domain) )]"),
